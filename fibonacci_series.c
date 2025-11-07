07e071e87fb20181c0f3d4a7a9870e5a18e613bf
Shubham Kumar <surajrxl06@gmail.com>
2025-11-07 16:18:49 +0530

    Add files via upload

---
+++ fibonacci_series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    int a = -1, b = 1, c;
+    for (int i = 1; i<= n; i++) {
+        c = a +b;
+        a = b;
+        b = c;
+        printf("%d ",c);
+    }
+    printf("\n");
+    return 0;
+}
